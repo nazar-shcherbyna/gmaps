34a39812860d6b9af3b932f19fb5a19f12429bbc
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2021-01-08 19:30:51 +0200

    fc

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function initMap() {
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 7,
+      center: { lat: 35.85, lng: -87.65 },
+    });
+    directionsRenderer.setMap(map);
+  
+    const onChangeHandler = function () {
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+    document.getElementById("surch").addEventListener("click", onChangeHandler);
+  }
+  
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    directionsService.route(
+      {
+        origin: {
+          query: document.getElementById("start").value,
+        },
+        destination: {
+          query: document.getElementById("end").value,
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (response, status) => {
+        if (status === "OK") {
+          directionsRenderer.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+  }
